4885f460fd850f24f9987cc6c54bd7719bb7a8df
Avinash <avinas.jha@sas.com>
2022-10-09 21:27:20 +0530

    Handled dimension mismatch error in the same function

--- compareTableArray.py
+++ compareTableArray.py
@@ -1,5 +1,7 @@
 # This function will compare both tables and return an array of differences.
 def compareArrayValues(a, b):
+    if len(a) != len(b) or len(a[0]) != len(b[0]):
+        return "Array dimension does not match."
     diff = []
     for i in range(len(a)):
         for j in range(len(a[i])):
