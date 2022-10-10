3f5b8744b812f4c3750476cf3a2fa0e8adf68b49
Bhavesh Sharma <bhavesh.sharma@sas.com>
2022-10-10 13:57:09 +0530

    Added Append QRT function

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from convertToMatrix import convertToStringMatrix
 from compareTableArray import compareArrayValues
-
+from Append_QRT import appendQRTSheets
 pathOfOldQRTSheets = '.\\old\\'
 pathOfNewQRTSheets = '.\\new\\'
 qrtNumber = 'Validated-S.01.01.14.xlsx'
 arr1 = ['A5','D10']
 arr2 = ['A5','D10']
+
+appendQRTSheets('.\\XBRL QRT','.\\Appended XBRL QRT')
+appendQRTSheets(".\\Solution QRT",".\\Appended Solution QRT")
 
 table1 = convertToStringMatrix(arr1,qrtNumber, pathOfOldQRTSheets)
 table2 = convertToStringMatrix(arr2, qrtNumber, pathOfNewQRTSheets)
