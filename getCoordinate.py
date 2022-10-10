9b209b9c58c6122d8b501cd6db01633823ee621b
Avinash <avinas.jha@sas.com>
2022-10-10 14:13:29 +0530

    Just one random helpful commit

--- getCoordinate.py
+++ getCoordinate.py
@@ -1,9 +1,3 @@
 # this function will get the coordinates of all the tables from a specific excel sheet
 
 # importing required libraries
-import openpyxl
-
-def getCoordinatesOfTable(pathOfQRTSheets, qrtNumber):
-    exact_path = pathOfQRTSheets+qrtNumber
-    wb = openpyxl.load_workbook(exact_path)
-    ws = wb[qrtNumber]
